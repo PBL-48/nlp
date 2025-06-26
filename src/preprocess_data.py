523ff548077f7d82da84c9b91386a6273f290c1e
PBL-48 <94658929PBL-48@users.noreply.github.com>
2025-06-26 21:14:23 +0900

    feat: 課題文の形態素解析を行う関数を追加

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -1,20 +1,26 @@
 from janome.tokenizer import Tokenizer
 
-from my_library.data_models import Token
+from my_library.data_models import Sentence, Token
 from my_library.load_save import load_data
 
 
-def preprocess_data():
+def tokenize_sentence() -> list[Sentence]:
+    """
+    課題文の文章を形態素解析するだけ。
+    """
+    return_list: list[Sentence] = []
     for sentence in load_data():
         sentence.word_list = [
             Token(
                 word=token.surface,
-                polarity=None,
+                polarity=1,
                 part_of_speech=token.part_of_speech.split(","),
-                not_effect_value=None,
-                emphasis_effect_value=None,
-                objectivity=None,
-                experience_or_evaluation=None,
+                not_effect_value=1,
+                emphasis_effect_value=1,
+                objectivity=1,
+                experience_or_evaluation=1,
             )
             for token in Tokenizer().tokenize(sentence.text)
         ]
+        return_list.append(sentence)
+    return return_list
