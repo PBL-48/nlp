b6598aa01c82a7bcea43b92342e43d97f0b7487d
Zen <zen050906@gmail.com>
2025-07-02 00:07:41 +0900

    ユニットテストのテスト

--- tests/test_data_models.py
+++ tests/test_data_models.py
@@ -2,44 +2,35 @@ import os
 import sys
 project_root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(project_root_path)
-import unittest
+import pytest
 from src.my_library.data_models import Token, Sentence, ModifiedToken, ModifiedSentence
 
-class TestModifiedToken(unittest.TestCase):
-    def setUp(self):
-        token = Token("美しい", 1.0, "adjective", 1.0, 1.0, 2.0, 2.0)
+class TestCalculator:
+    # 各テストメソッドの前に実行されるセットアップ処理
+    # self.calculator を初期化して、各テストで新しいインスタンスを使えるようにする
+    def setup_method(self):
+        token = Token(
+            word="美しい",
+            polarity=1.0,
+            part_of_speech="noun",
+            not_effect_value=1.0,
+            emphasis_effect_value=1.0,
+            objectivity=2.0,
+            experience_or_evaluation=2.0
+        )
         self.modified_token = ModifiedToken.modify_token(token)
 
     def test_calc_token_score(self):
-        """calc_token_scoreメソッドのテスト"""
-        self.assertEqual(self.modified_token.calc_token_score(), 4.0)
+        assert self.modified_token.calc_token_score() == 4.0
 
-    def test_subtract(self):
-        """subtractメソッドのテスト"""
-        self.assertEqual(self.calc.subtract(5, 2), 3)
-        self.assertEqual(self.calc.subtract(2, 5), -3)
-        self.assertEqual(self.calc.subtract(10, 0), 10)
+    # クラス全体で共有されるセットアップ処理 (オプション)
+    # def setup_class(cls):
+    #     print("\nSetting up TestCalculator class...")
 
-    def test_multiply(self):
-        """multiplyメソッドのテスト"""
-        self.assertEqual(self.calc.multiply(2, 3), 6)
-        self.assertEqual(self.calc.multiply(-1, 5), -5)
-        self.assertEqual(self.calc.multiply(0, 100), 0)
+    # クラス全体で共有されるティアダウン処理 (オプション)
+    # def teardown_class(cls):
+    #     print("Tearing down TestCalculator class.")
 
-    def test_divide(self):
-        """divideメソッドのテスト"""
-        self.assertEqual(self.calc.divide(6, 2), 3.0)
-        self.assertEqual(self.calc.divide(5, 2), 2.5)
-
-    def test_divide_by_zero(self):
-        """ゼロ除算のエラーハンドリングのテスト"""
-        with self.assertRaises(ValueError):
-            self.calc.divide(10, 0)
-
-    def tearDown(self):
-        """各テストメソッドの実行後に呼び出されるクリーンアップ処理（オプション）"""
-        # 必要であれば、ここでリソースの解放などを行う
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
+    # 各テストメソッドの後に実行されるティアダウン処理 (オプション)
+    # def teardown_method(self):
+    #     print(f"Teardown for {self.calculator} instance.")
