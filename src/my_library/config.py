ebbad20f4f0c2b3e7b2861969049183a3ddf7f89
PBL-48 <94658929PBL-48@users.noreply.github.com>
2025-07-03 08:49:14 +0900

    refactor: ルートパスを指定

--- src/my_library/config.py
+++ src/my_library/config.py
@@ -1,11 +1,14 @@
-DATA_FILE_PATH = "NLP/data/data.txt"
-POLARITY_DICT_PATH1 = "NLP/data/dictionary1.txt"
-POLARITY_DICT_PATH2 = "NLP/data/dictionary2.txt"
-OUTPUT_FILE_PATH = "NLP/data/preprocessed_data.json"
+import os
+
+project_root_path = os.path.dirname(os.path.dirname("NLP"))
+DATA_FILE_PATH = "data/data.txt"
+POLARITY_DICT_PATH1 = "data/dictionary1.txt"
+POLARITY_DICT_PATH2 = "data/dictionary2.txt"
+OUTPUT_FILE_PATH = "data/preprocessed_data.json"
 CONJ_EFFECT = 1.5
 UPPER_THRESHOLD = 0.5
 UNDER_THRESHOLD = -0.5
-#以下エラーを解消するための改善
+# 以下エラーを解消するための改善
 EMPHASIS_EFFECT = {
     "強調": 1.5,
     "強調": 1.5,
