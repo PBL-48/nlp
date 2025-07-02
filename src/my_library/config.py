6622d7fcd74168603cc457b9d99b85f68005c462
iwashita-108 <iwashita-yusuke392@g.ecc.u-tokyo.ac.jp>
2025-07-02 19:28:45 +0000

    否定・強調辞書

--- src/my_library/config.py
+++ src/my_library/config.py
@@ -5,3 +5,21 @@ OUTPUT_FILE_PATH = "NLP/data/preprocessed_data.json"
 CONJ_EFFECT = 1.5
 UPPER_THRESHOLD = 0.5
 UNDER_THRESHOLD = -0.5
+dict_not = {
+    "ない": -1.0
+}
+dict_emphasis = {
+    "とても": 2.0,
+    "一番": 3.0,
+    "どうしても": 2.0,
+    "最高": 3.0,
+    "少し": 0.5,
+    "なかなか": 1.5,
+    "特に": 2.0,
+    "久しぶり": 1.3,
+    "ずっと": 1.3,
+    "一日中": 1.3,
+    "早く": 1.3, 
+    "いっぱい": 1.3, 
+    "たくさん": 1.3
+}
